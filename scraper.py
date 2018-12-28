f3124ebdc18d53baa70f3ee79f973de9aa9cdd1e
James Vogel <jim.m.vogel@gmail.com>
2018-12-28 11:50:45 -0600

    added the ability to run this with a specified date

--- scraper.py
+++ scraper.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
-url = 'http://www.ercot.com/content/cdr/html/real_time_spp'
-df = pd.read_html(url)
+#example URL
+# http://www.ercot.com/content/cdr/html/20181227_real_time_spp
+
+def get_url(formatted_date=None):
+  if formatted_date is not None:
+      return f'http://www.ercot.com/content/cdr/html/{formatted_date}_real_time_spp'
+  return 'http://www.ercot.com/content/cdr/html/real_time_spp'
+
+df = pd.read_html(get_url())[0]
 df[0].to_csv('out.csv')
 
